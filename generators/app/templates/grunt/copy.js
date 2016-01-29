29c9e2ffd3feafa0cedc33da0dcb8f3eba78e58b
Guillermo de la Iglesia <zguillez@gmail.com>
2016-01-29 19:47:46 +0100

    favicon

--- generators/app/templates/grunt/copy.js
+++ generators/app/templates/grunt/copy.js
@@ -7,6 +7,12 @@ module.exports = function(grunt) {
 			dest: 'dist/',
 			expand: true
 		},
+		favicon: {
+			cwd: 'src/',
+			src: 'favicon.ico',
+			dest: 'dist/',
+			expand: true
+		},
 		index: {
 			cwd: 'src/',
 			src: 'index.html',
