4d6bb03a4a3b78e0c59c491e8058a0d8bdd2b3ca
Guillermo de la Iglesia <zguillez@gmail.com>
2016-01-29 17:35:37 +0100

    fix copy task

--- generators/app/templates/grunt/copy.js
+++ generators/app/templates/grunt/copy.js
@@ -74,4 +74,34 @@ module.exports = function(grunt) {
 			expand: true
 		}
 	});
+	var json, json2, lib, key, path;
+	json = grunt.file.readJSON('bower.json');
+	for (lib in json.dependencies) {
+		if (grunt.file.exists('bower_components/' + lib + '/bower.json')) {
+			json2 = grunt.file.readJSON('bower_components/' + lib + '/bower.json');
+			for (key in json2) {
+				if (key === 'main') {
+					if (typeof json2[key] === 'object') {
+						var paths = [];
+						for (var item in json2[key]) {
+							path = json2[key][item];
+							path = path.replace('./', '');
+							paths[item] = path;
+							grunt.config.set('copy.' + lib + '.cwd', 'bower_components/' + lib);
+							grunt.config.set('copy.' + lib + '.src', paths);
+							grunt.config.set('copy.' + lib + '.dest', 'dist/lib/');
+							grunt.config.set('copy.' + lib + '.expand', true);
+						}
+					} else {
+						path = json2[key];
+						path = path.replace('./', '');
+						grunt.config.set('copy.' + lib + '.cwd', 'bower_components/' + lib);
+						grunt.config.set('copy.' + lib + '.src', path);
+						grunt.config.set('copy.' + lib + '.dest', 'dist/lib/');
+						grunt.config.set('copy.' + lib + '.expand', true);
+					}
+				}
+			}
+		}
+	}
 };
